ec2ff9b2a74e32ac016e0bf8a170d6bd4a9ce0cc
doaa <73131139+doaa10@users.noreply.github.com>
2024-07-09 01:06:25 +0300

    review rate

--- script.js
+++ script.js
@@ -4,11 +4,28 @@ document.addEventListener("DOMContentLoaded", function() {
     
     const menuItem = document.querySelectorAll('.main_container_nav a');
     menuItem.forEach(link => {
-        console.log("Link pathname:", link.pathname.split('#')[0].split('?')[0]);
         if (link.pathname.split('#')[0].split('?')[0] === currentLocation) {
             link.classList.add("active");
-            console.log("Active class added to:", link.href);
             console.log(link)
         }
     });
+
+
+    const starContainer = document.getElementById('stars-container');
+    const rating = parseFloat(starContainer.getAttribute('data-rating'));
+    const maxStars = 5;
+
+    for (let i = 0; i < maxStars; i++) {
+        const star = document.createElement('i');
+        star.classList.add('fas');
+        if (i < Math.floor(rating)) {
+            star.classList.add('fa-star'); 
+        } else if (i < rating) {
+            star.classList.add('fa-star-half-alt'); 
+        } else {
+            star.classList.add('fa-star'); 
+            star.style.color = '#D3D3D3'; 
+        }
+        starContainer.appendChild(star);
+    }
 });
